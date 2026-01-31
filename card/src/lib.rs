393bf01f2e79aecc6860a29fd63606a11e34bed9
tokeiya3@Home <net_seed@hotmai.com>
2026-02-01 08:19:56 +0900

    sync

--- card/src/lib.rs
+++ card/src/lib.rs
@@ -8,3 +8,17 @@ pub use card::Card;
 pub use rank::{RANKS, Rank};
 pub use regular_card::RegularCard;
 pub use suit::{SUITS, Suit};
+
+pub fn all_pairs() -> impl Iterator<Item = (Rank, Suit)> {
+	SUITS
+		.iter()
+		.flat_map(|&suit| RANKS.iter().map(move |&rank| (rank, suit)))
+}
+
+pub fn all_cards(contain_joker: bool) -> impl Iterator<Item = Card> {
+	if contain_joker {
+		std::iter::once(Card::Joker).chain(all_pairs().map(Card::Regular))
+	} else {
+		all_pairs().map(Card::Regular)
+	}
+}
